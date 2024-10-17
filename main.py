6a62dc21dcfe1d35cec2c1f7f2d8a391b35807a8
MelnykIhor <melnyk70@gmail.com>
2024-10-17 15:54:45 +0300

    DZ_7_3

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-https://github.com/Melnyk70/lesson_7_3.git
+#ДЗ 7.3. Пошук підрядка
+# Функція second_index приймає як параметри 2 рядки. Вам необхідно знайти індекс другого входження шуканого рядка у рядку для пошуку.
+# Розберемо перший приклад, де необхідно знайти друге входження "s" в слові "sims". Якби нам треба було знайти її перше входження, то тут все просто: за допомогою функції index або find ми можемо дізнатися, що "s" - це перший символ у слові "sims", а значить індекс першого входження дорівнює 0. Але нам Необхідно визначити другу "s", а вона четверта за рахунком. Значить індекс другого входження (і у відповідь питання) дорівнює 3.
+# Рядок, який потрібно знайти, може складатися з кількох символів.
+# Input: Два рядки (String).
+# Output: Int or None
+# Приклади:
+# def second_index(text, some_str):
+#   pass
+# assert second_index("sims", "s") == 3, 'Test1'
+# assert second_index("find the river", "e") == 12, 'Test2'
+# assert second_index("hi", "h") is None, 'Test3'
+# assert second_index
+def second_index(text, some_str):
+   if text.count(some_str)>1:
+       ind=text.find(some_str)
+       ind1=text.find(some_str, ind + 1)
+   else:
+       ind1=None
+   return ind1
+assert second_index("sims", "s") == 3, 'Test1'
+assert second_index("find the river", "e") == 12, 'Test2'
+assert second_index("hi", "h") is None, 'Test3'
+assert second_index("Hello, hello", "lo") == 10, 'Test4'
+print('ОК')
